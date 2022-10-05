5484d2ad93e8a3929bcf8a3af7e6e727f73ab3f9
ayşegül <105206130+aysegulatas@users.noreply.github.com>
2022-10-05 23:48:58 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+function ucgen($say){  
+      for($i=0;$i<=$say;$i++){   
+    
+        $a=0; 
+          while($a<=$i){  
+             $a++;  
+            echo $a*0 ; 
+       
+   }     
+     echo   "<br>"; 
+   } 
+
+}
+ucgen(15);
+?>
+</body>
+</html>
